a97be2499111155870c36a32b8ae3e32db60ec02
Bryan <bryanchua04052004@gmail.com>
2026-08-25 16:51:44 +0800

    lvl 2: add, list

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,7 +1,16 @@
 import java.util.Scanner;
 
 public class Bob {
+    static void outputList(String[] input) {
+        int count = 0;
+        while(count < 100 && input[count] != null){
+            System.out.println(count+1 + ". " + input[count]);
+            count++;
+        }
+        return;
+    }
     public static void main(String[] args) {
+        // responses
         String intro = " _   _      _ _          ___ _                  ____        _     _ \n"
             + "| | | | ___| | | ___    |_ _|' _ __ ___       | __ )  ___ | |__ | |\n"
             + "| |_| |/ _ \\ | |/ _ \\    | || | '_ ` _ \\      |  _ \\ / _ \\| '_ \\| |\n"
@@ -19,13 +28,24 @@ public class Bob {
             + "|  Bye! Hope to see you again soon! |\n"
             + "|___________________________________|\n";
         
+        // variable creation
+        String[] input = new String[100];
+        int count = 0;
+
+        // chatbot
         System.out.println(intro);
         System.out.println(ask);
 
         Scanner scanner = new Scanner(System.in);
         String userCmd = scanner.nextLine();
         while(!userCmd.equals("bye")){
-            System.out.println(userCmd);
+            if(userCmd.equals("list")){
+                outputList(input);
+            }
+            else{
+                System.out.println("added: " + userCmd);
+                input[count++] = userCmd;
+            }
             userCmd = scanner.nextLine();
         }
         System.out.println(bye);
