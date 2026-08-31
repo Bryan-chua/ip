ff57d57d205208f18325fa0cb9b482f835ef2283
Bryan <bryanchua04052004@gmail.com>
2026-08-31 16:21:57 +0800

    codex skill

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    //...
+}
+
 public class Bob {
     static void outputList(String[] input) {
         int count = 0;
